ac474ce837744a43e42dd2859f5fd42fea920baa
crsimple <crisimple@foxmail.com>
2020-07-30 01:36:17 +0800

    ProcessLoop all

--- src/com/crisimple/base/ProcessLoopSample.java
+++ src/com/crisimple/base/ProcessLoopSample.java
@@ -44,5 +44,21 @@ public class ProcessLoopSample {
             System.out.println();
         }
         System.out.println("===== end =====");
+
+
+        // 打印 5 行的三角形
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j >= i; j--){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            for (int j = 1; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
     }
 }
