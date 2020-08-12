a7ca17b099294e3e294e285e16dbae5c1115d9ca
SF <sf@MacBook-Pro.local>
2020-08-12 19:29:07 +0800

    java method start

---
+++ src/com/crisimple/base/MethodDefine.java
@@ -0,0 +1,13 @@
+package com.crisimple.base;
+
+public class MethodDefine {
+    public static void main(String[] args) {
+        int c = add(1, 2);
+        System.out.println(c);
+    }
+
+    // 加法
+    public static int add(int a, int b){
+        return a + b;
+    }
+}
