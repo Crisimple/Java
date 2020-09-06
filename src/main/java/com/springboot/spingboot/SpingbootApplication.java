c65d1902f2c4ba9adfb4c96a0a53877b90db7ae0
crisimple <crisimple@qq.com>
2020-09-06 13:34:13 +0800

    SpringBoot Start

---
+++ src/main/java/com/springboot/spingboot/SpingbootApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.spingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingbootApplication.class, args);
+	}
+}
