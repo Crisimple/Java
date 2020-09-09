fb6f466337e41d0b700e2813421af3df854557cc
crisimple <crisimple@qq.com>
2020-09-10 01:16:15 +0800

    yaml inject to Class value

--- src/main/java/com/springboot/SpringbootStartApplication.java
+++ src/main/java/com/springboot/SpringbootStartApplication.java
@@ -3,10 +3,12 @@ package com.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 标注这个类是一个 SpringBoot 的应用
 @SpringBootApplication
 public class SpringbootStartApplication {
 
 	public static void main(String[] args) {
+		// 将 SpringBoot 应用程序启动
 		SpringApplication.run(SpringbootStartApplication.class, args);
 	}
 
--- src/test/java/com/springboot/SpringbootStartApplicationTests.java
+++ src/test/java/com/springboot/SpringbootStartApplicationTests.java
@@ -1,13 +1,21 @@
 package com.springboot;
 
+import com.springboot.project.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootStartApplicationTests {
-
+	
+	@Autowired  // 将测试类自动注入进来
+//	private Dog dog;
+	private Person person;
+	
 	@Test
 	void contextLoads() {
+		// 查看下狗狗对象的值是否被传参成功
+		System.out.println(person);
 	}
 
 }
