517bf25d85ce5d48a4d422960575dc7fa5c59e72
secoder <crisimple@qq.com>
2021-07-12 21:34:50 +0800

    druid study

--- spring_boot/src/test/java/com/secode/spring_boot/utils/SpringBootDataJdbcApplicationTest.java
+++ spring_boot/src/test/java/com/secode/spring_boot/utils/SpringBootDataJdbcApplicationTest.java
@@ -1,5 +1,6 @@
 package com.secode.spring_boot.utils;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,12 +24,16 @@ public class SpringBootDataJdbcApplicationTest {
     @Test
     public void contextLoads() throws SQLException {
 
-        // 查看下默认数据源
+        // 查看下默认数据源（切换到了阿里的druid数据源，看是否成功）
         System.out.printf("dataSource.getClass(): ", dataSource.getClass());
 
         // 获得连接
         Connection connection = dataSource.getConnection();
         System.out.printf("connection: ", connection);
+    
+        DruidDataSource druidDataSource = (DruidDataSource) dataSource;
+        System.out.println("druidDataSource 数据源最大连接数：" + druidDataSource.getMaxActive());
+        System.out.println("druidDataSource 数据源初始化连接数：" + druidDataSource.getInitialSize());
 
         // 关闭连接
         connection.close();
